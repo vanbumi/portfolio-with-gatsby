76db3507d2925b5c03d59273e18e6ea20211baf4
vanbumi <jsp.dio@gmail.com>
2020-06-17 20:55:01 +0700

    87 - Jobs Query

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -4,6 +4,23 @@ import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
+const query = graphql`
+  {
+    allStrapiJobs(sort: { fields: strapiId, order: ASC }) {
+      nodes {
+        strapiId
+        company
+        date
+        position
+        desc {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
 const Jobs = () => {
   return <h2>jobs component</h2>
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,12 @@ import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 export default () => {
-  return <Layout>
-    <Hero/>
-    <Services/>
-  </Layout>
+  return (
+    <Layout>
+      <Hero />
+      <Services />
+      <Jobs />
+    </Layout>
+  )
 }
 // ...GatsbyImageSharpFluid
