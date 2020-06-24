097e7e97dfe75340cc0390d662140b2865bd2f78
vanbumi <jsp.dio@gmail.com>
2020-06-24 21:24:52 +0700

    109 - Sidebar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,6 +7,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
+      <Sidebar />
       {children}
       <Footer />
     </>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,19 @@ import Links from "../constants/links"
 import SocialLinks from "../constants/socialLinks"
 import { FaTimes } from "react-icons/fa"
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  const isOpen = false
+
+  return (
+    <aside className={`sidebar ${isOpen ? "show-sidebar" : ""} `}>
+      <button className="close-btn">
+        <FaTimes />
+      </button>
+      <div className="side-container">
+        <Links styleClass="sidebar-links" />
+        <SocialLinks styleClass="sidebar-icons" />
+      </div>
+    </aside>
+  )
 }
 
 export default Sidebar
