84482be6bb7ea5cd8dfca753244e76f8d5e24d01
vanbumi <jsp.dio@gmail.com>
2020-06-26 15:02:28 +0700

    112 -  Error Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,18 @@ import { Link } from "gatsby"
 import SEO from "../components/SEO"
 
 const Error = () => {
-  return <h2>error page</h2>
+  return (
+    <Layout>
+      <main className="error-page">
+        <div className="error-container">
+          <h1>Maaf Halaman yang dicari tidak ada</h1>
+          <Link to="/" className="btn">
+            back to home page
+          </Link>
+        </div>
+      </main>
+    </Layout>
+  )
 }
 
 export default Error
