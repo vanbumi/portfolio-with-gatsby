d1565cc39baa08c09fe26ad3ab26798d4359d57f
vanbumi <jsp.dio@gmail.com>
2020-06-30 14:02:31 +0700

    123 - SEO Basic Setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
+import SEO from "../components/SEO"
 
 export default ({ data }) => {
   // console.log(data)
@@ -16,6 +17,7 @@ export default ({ data }) => {
 
   return (
     <Layout>
+      <SEO title="Home" description="this is our home page" />
       <Hero />
       <Services />
       <Jobs />
