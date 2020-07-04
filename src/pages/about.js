7f95fadc6e97c47fc2819557daa16b3311d5a652
vanbumi <jsp.dio@gmail.com>
2020-07-04 17:18:33 +0700

    125 - SEO All Pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,8 @@ import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Title from "../components/Title"
 import Image from "gatsby-image"
+import SEO from "../components/SEO"
+
 const About = ({
   data: {
     about: { nodes },
@@ -13,6 +15,7 @@ const About = ({
 
   return (
     <Layout>
+      <SEO title="About" description="about webdev" />
       <section className="about-page">
         <div className="section-center about-center">
           <Image fluid={image.childImageSharp.fluid} className="about-img" />
