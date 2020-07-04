f13eeeb1405537f5973198582d9b282e3184cdf7
vanbumi <jsp.dio@gmail.com>
2020-07-04 15:04:11 +0700

    124 - SEO Query

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -2,10 +2,35 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
+const query = graphql`
+  {
+    site {
+      siteMetadata {
+        author
+        siteDesc: description
+        image
+        siteUrl
+        siteTitle: title
+        twitterUsername
+      }
+    }
+  }
+`
+
 const SEO = ({ title, description }) => {
+  const { site } = useStaticQuery(query)
+  const {
+    siteDesc,
+    siteTitle,
+    siteUrl,
+    image,
+    twitterUsername,
+  } = site.siteMetadata
+
   return (
-    <Helmet htmlAttributes={{ lang: "en" }} title={title}>
-      <meta name="description" content={description} />
+    <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
+      <meta name="description" content={description || siteDesc} />
+      <meta name="image" content={image} />
     </Helmet>
   )
 }
