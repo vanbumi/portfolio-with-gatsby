a31ebc7fee63d1bc6012131c89c2d2fad64bf2d3
vanbumi <jsp.dio@gmail.com>
2020-07-24 16:41:09 +0700

    Update SEO component

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -31,6 +31,12 @@ const SEO = ({ title, description }) => {
     <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
       <meta name="description" content={description || siteDesc} />
       <meta name="image" content={image} />
+      {/** twitter cards */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={twitterUsername} />
+      <meta name="twitter:tittle" content={siteTitle} />
+      <meta name="twitter:description" content={siteDesc} />
+      <meta name="twitter:image" content={`${siteUrl}${image}`} />
     </Helmet>
   )
 }
